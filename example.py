c822627cc71f2ffdcea729613a6690874e41be86
Sanat Sharma <sanatcolumban@gmail.com>
2022-06-12 11:54:52 -0500

    Update Example to pay out royalties to original nft creator

--- example.py
+++ example.py
@@ -2,11 +2,14 @@ from time import time, sleep
 
 from algosdk import account, encoding
 from algosdk.logic import get_application_address
+from algosdk.future import transaction
 from auction.operations import createAuctionApp, setupAuctionApp, placeBid, closeAuction
 from auction.util import (
     getBalances,
     getAppGlobalState,
     getLastBlockTimestamp,
+    checkAssetPossession,
+    waitForTransaction
 )
 from auction.testing.setup import getAlgodClient
 from auction.testing.resources import (
@@ -21,23 +24,51 @@ def simple_auction():
 
     print("Generating temporary accounts...")
     creator = getTemporaryAccount(client)
+    originalSeller = getTemporaryAccount(client)
     seller = getTemporaryAccount(client)
     bidder = getTemporaryAccount(client)
 
-    print("Alice (seller account):", seller.getAddress())
+    print("Alice (NFT Creator account):", originalSeller.getAddress())
+    print("Jack (seller): ", seller.getAddress())
     print("Bob (auction creator account):", creator.getAddress())
     print("Carla (bidder account)", bidder.getAddress(), "\n")
 
     print("Alice is generating an example NFT...")
     nftAmount = 1
-    nftID = createDummyAsset(client, nftAmount, seller)
+    nftID = createDummyAsset(client, nftAmount, originalSeller)
     print("The NFT ID is", nftID)
-    print("Alice's balances:", getBalances(client, seller.getAddress()), "\n")
+    print("Alice's balances:", getBalances(client, originalSeller.getAddress()), "\n")
+    print("Alice NFT Ownership status : ", checkAssetPossession(client, originalSeller.getAddress(), nftID))
+
+    initialCreatorAmount = getBalances(client, originalSeller.getAddress())[0]
+
+    print("Alice sold the NFT to Jack...")
+    print("Jack is opting into the NFT")
+    optInToAsset(client, nftID, seller)
+
+    payTxn = transaction.AssetTransferTxn(
+        sender=originalSeller.getAddress(),
+        receiver=seller.getAddress(),
+        amt=1,
+        index=nftID,
+        sp=client.suggested_params()
+    ) 
+
+    transaction.assign_group_id([payTxn])
+
+    signedPayTxn = payTxn.sign(originalSeller.getPrivateKey())
+
+    client.send_transactions([signedPayTxn])
+
+    waitForTransaction(client, signedPayTxn.get_txid())
+    print("Alice's NFT Ownership status : ", checkAssetPossession(client, originalSeller.getAddress(), nftID))
+    print("Jack's NFT Ownership status : ", checkAssetPossession(client, seller.getAddress(), nftID))
 
     startTime = int(time()) + 10  # start time is 10 seconds in the future
     endTime = startTime + 30  # end time is 30 seconds after start
     reserve = 1_000_000  # 1 Algo
     increment = 100_000  # 0.1 Algo
+    royaltyPercentage = 10
     print("Bob is creating an auction that lasts 30 seconds to auction off the NFT...")
     appID = createAuctionApp(
         client=client,
@@ -48,6 +79,8 @@ def simple_auction():
         endTime=endTime,
         reserve=reserve,
         minBidIncrement=increment,
+        royaltyPercentage=royaltyPercentage,
+        nftCreator=originalSeller.getAddress()
     )
     print(
         "Done. The auction app ID is",
@@ -57,7 +90,7 @@ def simple_auction():
         "\n",
     )
 
-    print("Alice is setting up and funding NFT auction...")
+    print("Jack is setting up and funding NFT auction...")
     setupAuctionApp(
         client=client,
         appID=appID,
@@ -70,7 +103,7 @@ def simple_auction():
 
     sellerBalancesBefore = getBalances(client, seller.getAddress())
     sellerAlgosBefore = sellerBalancesBefore[0]
-    print("Alice's balances:", sellerBalancesBefore)
+    print("Jack's balances:", sellerBalancesBefore)
 
     _, lastRoundTime = getLastBlockTimestamp(client)
     if lastRoundTime < startTime + 5:
@@ -98,7 +131,7 @@ def simple_auction():
         print("Waiting {} seconds for the auction to finish\n".format(waitTime))
         sleep(waitTime)
 
-    print("Alice is closing out the auction\n")
+    print("Jack is closing out the auction\n")
     closeAuction(client, appID, seller)
 
     actualAppBalances = getBalances(client, get_application_address(appID))
@@ -110,10 +143,15 @@ def simple_auction():
     assert bidderNftBalance == nftAmount
 
     actualSellerBalances = getBalances(client, seller.getAddress())
-    print("Alice's balances after auction: ", actualSellerBalances, " Algos")
+    print("Jack's balances after auction: ", actualSellerBalances, " Algos")
     actualBidderBalances = getBalances(client, bidder.getAddress())
     print("Carla's balances after auction: ", actualBidderBalances, " Algos")
     assert len(actualSellerBalances) == 2
+    print("NFT Creator's royalty")
+    actualNFTCreatorBalances = getBalances(client, originalSeller.getAddress())
+    print("Alice's balances after auction: ", actualNFTCreatorBalances, " Algos")
+    print("Royalties paid to NFT Creator: ", actualNFTCreatorBalances[0] - initialCreatorAmount)
+
     # seller should receive the bid amount, minus the txn fee
     assert actualSellerBalances[0] >= sellerAlgosBefore + bidAmount - 1_000
     assert actualSellerBalances[nftID] == 0
